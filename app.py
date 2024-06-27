509ff6d1bc2bdc1e64cbd7884d7ee7960403f8b5
Genomicsinsights <genomicsinsights@gmail.com>
2024-06-27 08:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def create_download_link(data, filename, mime):
 
 page = st.sidebar.radio("Choose a page", ["Home", "About", "Help"])
 st.title("VaxOptiML")
-st.write("An integrated pipeline designed to enhance epitope prediction and prioritization")
+st.write("An integrated pipeline designed to enhance Cancer epitope prediction and prioritization")
 st.image("logo.png", width=200)
 
 text = [
